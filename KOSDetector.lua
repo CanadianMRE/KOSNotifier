a6d5ec78d1e73cbf995a44b18b6fd31533454cf5
CanadianMRE <jaymen100@gmail.com>
2021-03-18 15:58:21 -0600

    Update KOSDetector.lua

--- KOSDetector.lua
+++ KOSDetector.lua
@@ -2,31 +2,33 @@ local StarterGui = game:GetService("StarterGui")
 local CoreGui = game:GetService("CoreGui")
 local Players;
 local Player;
-local sound;
 
 repeat wait() until game.IsLoaded and game:GetService("Players") ~= nil
 Players = game:GetService("Players")
 repeat wait() until Players.LocalPlayer ~= nil
 Player = Players.LocalPlayer
-
 repeat wait() until Player.Character ~= nil
-wait(1)
+repeat wait() until Player.Character.HumanoidRootPart ~= nil
+local sound = Instance.new("Sound", Player.Character.HumanoidRootPart)
+sound.Name = math.random(100000, 10000000)
+sound.SoundId = "rbxassetid://6223512980"
 
-print("test")
+-- House KOS List
+local houseKOS = {
+	"XIV"
+}
 
-repeat wait() until Player.Character.HumanoidRootPart.RagingDemon ~= nil
-sound = Player.Character.HumanoidRootPart.RagingDemon
-print("test2")
-
--- Players on KOS
-local bannedplayers = {
+-- KOS list
+local playerKOS = {
 	"AltroScraps",
 	"aahoura",
 	"koehiii",
 	"guest1455386",
-	"XANCYUO"
+	"XANCYUO",
+	"Lanesmoe03"
 }
 
+-- Notifier
 local function notify(plr)
 	sound:Play()
 	StarterGui:SetCore("SendNotification", {
@@ -37,15 +39,17 @@ local function notify(plr)
 	})
 end
 
+-- Notifies if players are found on join
 for i,v in pairs(Players:GetPlayers()) do
-print("checkedplayer")
-	if table.find(bannedplayers, v.Name) then
+	print("checkedplayer")
+	if table.find(playerKOS, v.Name) then
 		notify(v.Name)
 	end
 end
 
+-- Notifies if a player on the list joins your game
 Players.PlayerAdded:Connect(function(Player)
-	if table.find(bannedplayers, Player.Name) then
+	if table.find(playerKOS, Player.Name) then
 		notify(Player.Name)
 	end
 end)
